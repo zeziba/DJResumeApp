6aec9c498cc0f446c2c1bdecebf61e7b1e9a6a8a
Charles Owen Engen <owenengen@gmail.com>
2022-04-14 09:28:43 -0600

    Added skills

--- app/index/models.py
+++ app/index/models.py
@@ -1,9 +1,14 @@
-from pyexpat import model
+from distutils.text_file import TextFile
 from django.db import models
 
 class WorkExperience(models.Model):
-    company_name = models.TextField()
+    company_name = models.CharField(max_length=40)
     start_date = models.DateField()
     end_date = models.DateField()
-    description = models.TextField()
-    title = models.TextField()
+    description = models.TextField(max_length=800)
+    title = models.CharField(max_length=60)
+    
+class Skills(models.Model):
+    skill_name = models.CharField(max_length=40)
+    description = models.TextField(max_length=120)
+    example = models.URLField()
--- app/index/urls.py
+++ app/index/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('<int:work_id>/', view=views.work_experience_detail, name='work_experience'),
+    path('<int:skill_id>/', view=views.skill_detail, name='skills'),
 ]
