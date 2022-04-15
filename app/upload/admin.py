7a279ce0d3eb01cc1f17d1c3244692ef18e5d442
Charles Owen Engen <owenengen@gmail.com>
2022-04-15 04:31:11 -0600

    Changed to require login to submit

--- app/upload/admin.py
+++ app/upload/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import SubmitImage
+
+admin.site.register(SubmitImage)
--- app/upload/views.py
+++ app/upload/views.py
@@ -1,7 +1,12 @@
+import imp
 from django.shortcuts import render
 from django.core.files.storage import FileSystemStorage
+from django.contrib.admin.views.decorators import staff_member_required
+
+from .models import SubmitImage
 
 
+@staff_member_required
 def image_upload(request):
     if request.method == "POST" and request.FILES["image_file"]:
         image_file = request.FILES["image_file"]
@@ -9,6 +14,7 @@ def image_upload(request):
         filename = fs.save(image_file.name, image_file)
         image_url = fs.url(filename)
         print(image_url)
+        SubmitImage.objects.create(location=image_url)
         return render(request, "upload/partials/upload.html", {
             "image_url": image_url
         })
