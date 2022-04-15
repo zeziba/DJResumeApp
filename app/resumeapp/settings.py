e6d75b12267298bc5e036f2696598688b70b07f9
Charles Owen Engen <owenengen@gmail.com>
2022-04-15 17:17:32 -0600

    Updated messages and compose

--- app/resumeapp/settings.py
+++ app/resumeapp/settings.py
@@ -21,17 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
-if not SECRET_KEY:
-    SECRET_KEY = 'django-insecure-2&u-8+t!yd_-b%9l$#y)gj9+hm_xsgo8p1kd7#03d*3hws3d@d'
+SECRET_KEY = os.environ.get("SECRET_KEY", 'django-insecure-2&u-8+t!yd_-b%9l$#y)gj9+hm_xsgo8p1kd7#03d*3hws3d@d')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(os.environ.get("DEBUG", default=0))
 
-try:
-    ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
-except AttributeError:
-    ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS", []).split(" ")
 
 
 # Application definition
