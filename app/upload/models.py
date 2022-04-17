8f9302c828d167ad9865c8a37ef6da3a0b392b69
Charles Owen Engen <owenengen@gmail.com>
2022-04-16 22:14:50 -0600

    Deleted unneed property

--- app/upload/models.py
+++ app/upload/models.py
@@ -7,11 +7,6 @@ UPLOAD_LOCATION = f'uploads/'
 class Images(models.Model):
     image = models.ImageField(upload_to=UPLOAD_LOCATION)
 
-    @property
-    def image_url(self):
-        if self.image and hasattr(self.image, 'url'):
-            return self.image.url
-
     def __str__(self) -> str:
         if self.image and hasattr(self.image, 'url'):
             return f"{(len(self.image.url) > MAX_CHAR_LENGTH) * '...'}{self.image.url[-MAX_CHAR_LENGTH:]}"
